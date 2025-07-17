802133cd37d7b2777f8b50986afaa37e3b54de6f
PhantomBlackOP <trevor.o.grech@gmail.com>
2025-07-17 16:17:06 +0200

    Update generate_feed.py

--- scripts/generate_feed.py
+++ scripts/generate_feed.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add repo root to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import json
 from server.algos.animeai import animeai_algo
 from server.storage import get_all_posts
